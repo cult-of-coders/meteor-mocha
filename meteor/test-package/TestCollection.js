ea1a316657caf70e968b738ffd619ea01ed5fec9
decaffeinate <jfemail3@gmail.com>
2018-08-06 16:00:57 +0200

    decaffeinate: Run post-processing cleanups on MochaRunner.coffee and 12 other files

--- meteor/test-package/TestCollection.js
+++ meteor/test-package/TestCollection.js
@@ -1,3 +1,5 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 const {Mongo} = require("meteor/mongo");
 
 const TestCollection = new Mongo.Collection('test.collection');
